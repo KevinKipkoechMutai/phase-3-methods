7acecfda4b518f54bc8f41fd4ecba359774266ab
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-18 11:18:33 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+# method greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#greet method with one argument
+def greet name
+    puts "Hello, #{name}!"
+end
+
+#method greet_with_default with one argument
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#add method
+def add(num1, num2)
+    return num1 + num2
+end
+
+#halve method
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    num / 2
+end
+
